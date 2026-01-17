01a7bd0a0bcdfda017e2cde0b99e20b47e91b213
DearDarlin <darinakovalenko78@gmail.com>
2026-01-17 22:26:46 +0200

    I do task1, i make date day of the year

--- Homework1ASP/Pages/Index.cshtml.cs
+++ Homework1ASP/Pages/Index.cshtml.cs
@@ -3,18 +3,21 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Homework1ASP.Pages
 {
-    public class IndexModel : PageModel
+    public class IndexModel : PageModel//Оголошена в класі, щоб вона була доступна на сторінці
     {
         private readonly ILogger<IndexModel> _logger;
+
+        public int DayOfYear { get; set; } //Властивість моделі сторінки
 
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
         }
 
-        public void OnGet()
+        public void OnGet()//Користувач зайшов на сторінку
+                           //Сервер отримує запит і виконує метод OnGet
         {
-
+            DayOfYear = DateTime.Now.DayOfYear;
         }
     }
 }
