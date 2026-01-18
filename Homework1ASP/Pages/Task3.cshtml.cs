bd761cf392ab2b0c56438e83ee339cb4ea584e43
DearDarlin <darinakovalenko78@gmail.com>
2026-01-18 21:12:44 +0200

    Task3 was done, i do short information about restaurant

---
+++ Homework1ASP/Pages/Task3.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Homework1ASP.Pages
+{
+    public class Task3Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
